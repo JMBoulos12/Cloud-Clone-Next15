706dbdf35c8d3b847bbb56ba1aec28efc18014ec
JMBoulos <jeanmarc8boulos@gmail.com>
2025-06-07 23:29:12 +0300

    setup project, OTP auth, file upload, React Query integration -07/06/2025

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import type { auth } from "@/lib/better-auth/auth";
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import { Session } from "./lib/better-auth/auth-types";
+
+async function getMiddlewareSession(req: NextRequest) {
+  const { data: session } = await axios.get<Session>("/api/auth/get-session", {
+    baseURL: req.nextUrl.origin,
+    headers: {
+      cookie: req.headers.get("cookie") || "", // Forward the cookies from the request
+    },
+  });
+
+  return session;
+}
+
+export async function authmiddleware(req: NextRequest) {
+  const Session = getMiddlewareSession(req);
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
